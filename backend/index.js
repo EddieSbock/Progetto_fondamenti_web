45d5cb302ccf46806436819784ba3c8cfb0fd2c5
Andre <andrea.lamedi25@gmail.com>
2026-08-22 20:22:38 +0200

    verifica connessione frontend-backend

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,8 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Sono il backend)")
+app.get("/api/hello", (req, res) => {
+    res.send("Sono il backend")
 });
 app.listen(port, () => {
     console.log(`Server in ascolto su ${port}`)
