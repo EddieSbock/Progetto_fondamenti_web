2c811003bc3333c9be2e80312fd8e3a1f335f4be
Andre <andrea.lamedi25@gmail.com>
2026-08-31 21:46:36 +0200

    Aggiunta prima bozza pagina home

--- frontend/src/pagine/Home.jsx
+++ frontend/src/pagine/Home.jsx
@@ -1,4 +1,67 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Contesto } from "../contesto/AuthContext";
+import "./Home.css";
+import "../componenti/PostCard"
+import { PostCard } from "../componenti/PostCard";
+
 export default function Home() {
+
+    const [post, setPost]= React.useState([])
+    const [errore, setErrore] = React.useState(null)
+    const [ricerca, setRicerca] = React.useState("")
+
+React.useEffect(() => {
+    const handlePost = async () => {
+        try {
+            const HomePost = await axios.get("http://localhost:3000/api/post?limit=10")
+
+            console.log(HomePost.data.message)
+            setPost(HomePost.data.postati)
+        } catch (errore) {
+            console.log(errore)
+            setErrore("Problema col recuperare i post")
+        }
+    };
+    handlePost();
+}, [])
+
+
+
     return (
-        <h1>Home</h1>
-    )}
+        <div className="home">
+            <header className="barra-home">
+                <div className="barra-ricerca">
+                    <input
+                        className="ricerca-input" 
+                        type="text"
+                        placeholder="Cerca..."
+                        value={ricerca}
+                        onChange={(e)=> setRicerca(e.target.value)}
+                    />
+                </div>
+                <div className="barra-utility">
+                    <span>{post.length} Post</span>
+                </div>
+            </header>
+            <div className="corpo-home">
+                <div className="corpo-post">
+                {post.map((post) => (
+                    <PostCard key={post._id} post={post}/>)
+                )}
+                </div>
+                <aside className="corpo-sidebar">
+                    <div className="sidebar-sezione">
+                        <h3>Categorie</h3>
+                    </div>
+
+                    <div className="sidebar-sezione">
+                        <h3>Top Recensioni</h3>
+                    </div>
+                </aside>
+            </div>
+            
+        </div>
+    )
+}
